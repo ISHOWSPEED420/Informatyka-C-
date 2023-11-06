1a3b7f25a174f76f50e4e56aac644209e4f3332c
ISHOWSPEED420 <jakubdudek62@gmail.com>
2023-11-06 12:03:05 +0100

    Komentarz

---
+++ temat 3 pEtle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main(){
+  //for(stanPoczatkowy; warunekKoncowy; krok)
+  for(int i=0; i <5; i++){
+  	cout << "Iteracja petli: " << i<< endl;
+  }
+
+  // zagniedzienie petli
+  for(int i = 0; i < 5; i++){
+  	for(int j = 0; j < 5; j++){
+  		cout << "i:"<< i;
+  		cout << endl;
+	  }
+  	
+  	
+  	
+  }
+
+
+cout << endl;
+system("pause");
+}
